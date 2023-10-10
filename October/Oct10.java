208808304a254dc1a8fc0b8260f4ba4c2f6388d1
druvkotwani <druvkotwani12@gmail.com>
2023-10-10 22:07:14 +0530

    oct 10th

---
+++ October/Oct10.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/description/?envType=daily-question&envId=2023-10-10
+    class Solution {
+        public int minOperations(int[] nums) {
+            int n = nums.length;
+            int ans = n;
+            
+            HashSet<Integer> unique = new HashSet<>();
+            for (int num : nums) {
+                unique.add(num);
+            }
+            
+            int[] newNums = new int[unique.size()];
+            int index = 0;
+            
+            for (int num : unique) {
+                newNums[index++] = num;
+            }
+                
+            Arrays.sort(newNums);
+            
+            int j = 0;
+            for (int i = 0; i < newNums.length; i++) {
+                while (j < newNums.length && newNums[j] < newNums[i] + n) {
+                    j++;
+                }
+    
+                int count = j - i;
+                ans = Math.min(ans, n - count);
+            }
+            
+            return ans;
+        }
+    }
+
